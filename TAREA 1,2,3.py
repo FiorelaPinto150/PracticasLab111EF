6671c2285cbaa37fd60ced4d4374853e692d258c
Fiorela Pinto <66995032+FiorelaPinto150@users.noreply.github.com>
2020-07-08 16:00:35 -0400

    Add files via upload

---
+++ TAREA 1,2,3.py
@@ -0,0 +1,230 @@
+# Programa con menu
+# Iniciar una lista de estudiantes
+# Mostrar la lista
+# ALTAS
+# BAJAS
+# MODIFICACIONES
+# CORE y REPOSITORIO
+#TODO: mockups
+
+estudiantesNombres = ['Arun', 'Daniel']
+estudiantesApellidos = ['Limachi', 'Rivera']
+estudiantesCI = ['8463443', '6156166']
+estudiantesFechaNacimiento = [1994, 2000]
+estudiantesTelefono = ['71201944', '72002015']
+
+
+
+def mostrarMenu():
+    print('')
+    print('♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠')
+    print('       ♦Ingrese la opcion requerida: ')
+    print('♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫')
+    print('    *1. Mostrar la lista')
+    print('♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫')
+    print('    *2. Adicionar un estudiante')
+    print('♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫')
+    print('    *3. Modificar un estudiante')
+    print('♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫')
+    print('    *4. Eliminar un estudiante')
+    print('♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫')
+    print('    *5. Consulta Salida cuarentena')
+    print('♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫')
+    print('    *6. Consulta de Mesa Electoral ')
+    print('♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫♫')
+    print('    0. Salir')
+    print('♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠')
+    print('')
+
+
+def mostrarEstudiantes():
+    print(estudiantesNombres)
+    print(estudiantesApellidos)
+    print(estudiantesCI)
+    print(estudiantesFechaNacimiento)
+    print(estudiantesTelefono)
+
+
+def adicionarEstudiante():
+    # nombres
+    print("♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣")
+    nuevoNombre = input('Ingrese los nombres del estudiante: ')
+    estudiantesNombres.append(nuevoNombre)
+    # apellidos
+    estudiantesApellidos.append(input('Ingrese los apellidos del estudiante: '))
+    # CI
+    estudiantesCI.append(input('Ingrese el CI del estudiante: '))
+    # Fecha de nacimiento
+    estudiantesFechaNacimiento.append(input('Ingrese la fecha de nacimiento del estudiante: '))
+    # Telefono
+    estudiantesTelefono.append(input('Ingrese el telefono del estudiante: '))
+    print('Se han guardado los datos del nuevo estudiante')
+    print("♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣")
+
+
+def modificarEstudiante():
+    print("♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥")
+    print("Presione 0 si desea salir de la opcion modificacion!!!")
+    aux1=input("Introduce en parametro del estudiante que quieras modificar(Parametro C.I es CI):").lower()
+    posicion1 = estudiantesCI.index(input('Ingrese el Carnet de Identidad del estudiante a modificar: '))
+    print("♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥")
+    while aux1!="0":
+        if aux1=="nombre":
+            aux2=estudiantesNombres.pop(posicion1)
+            nombreNuevo=input("Ingrese el Nuevo Nombre:")
+            estudiantesNombres.insert(posicion1,nombreNuevo)
+            print(mostrarEstudiantes())
+            print("♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥6156166♥♥♥♥")
+        if aux1=="apellido":
+            aux2=estudiantesApellidos.pop(posicion1)
+            apellidoNuevo=input("Ingrese el Nuevo Apellido:")
+            estudiantesApellidos.insert(posicion1,apellidoNuevo)
+            print(mostrarEstudiantes())
+            print("♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥")
+        if aux1=="CI":
+            aux2=estudiantesCI.pop(posicion1)
+            CINuevo=input("Ingrese el Nuevo C.I:")
+            estudiantesCI.insert(posicion1,CINuevo)
+            print(mostrarEstudiantes())
+            print("♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥")
+        if aux1=="Fecha de Nacimiento":
+            aux2=estudiantesFechaNacimiento.pop(posicion1)
+            FechaNuevo=input("Ingrese el Nuevo C.I:")
+            estudiantesFechaNacimiento.insert(posicion1,FechaNuevo)
+            print(mostrarEstudiantes())
+            print("♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥")
+        if aux1=="telefono":
+            aux2=estudiantesTelefono.pop(posicion1)
+            TelefNuevo=input("Ingrese el Nuevo Apellido:")
+            estudiantesTelefono.insert(posicion1,TelefNuevo)
+            print(mostrarEstudiantes())
+            print("♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥")
+        print("Presione 0 si desea salir de la opcion modificacion o ingrese 1 si requiere cambiar de estudiante:!!!")
+        aux1 = input("Introduce en parametro del estudiante que quieras modificar o una opcion:").lower()
+        print("♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥")
+        if aux1=="1":
+            posicion1 = estudiantesCI.index(input('Ingrese el Carnet de Identidad del estudiante a modificar: '))
+            print("♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥")
+
+
+def eliminarEstudiante():
+    # ciBorrar = input('Ingrese el Carnet de Identidad del estudiante a borrar: ')
+    # posicion = estudiantesCI.index(ciBorrar)
+    print("♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥")
+    posicion = estudiantesCI.index(input('Ingrese el Carnet de Identidad del estudiante a borrar: '))
+
+    estudiantesNombres.pop(posicion)
+    estudiantesApellidos.pop(posicion)
+    estudiantesCI.pop(posicion)
+    estudiantesFechaNacimiento.pop(posicion)
+    estudiantesTelefono.pop(posicion)
+    print("☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼")
+    print('                 Se ha eliminado al estudiante.')
+    print("☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼☼")
+
+# Ingresar un dia y mostrar a los estudiantes que pueden salir ese dia
+def consultaSalidaCuarentena():
+    # Lun Mie Vie   impares
+    # Mar Jue Sab   pares
+    dia = input('Ingrese el dia que se desea validar (lun mie vie mar jue sab): ').lower()
+    comparador=-1
+    # LUN lun Lun LuN
+    if dia in ['lun', 'mie', 'vie']:
+        comparador=1
+        print('Los estudiantes que pueden salir son: ')
+    elif dia in ['mar', 'jue', 'sab']:
+        comparador=0
+        print('Los estudiantes que pueden salir son: ')
+    else:
+        print('Dia no registrado')
+    for i in range(len(estudiantesNombres)):
+        if int(estudiantesCI[i]) % 2 == comparador:
+            print("♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦")
+            print(estudiantesNombres[i])
+            print(estudiantesApellidos[i])
+            print(estudiantesCI[i])
+            print(estudiantesFechaNacimiento[i])
+            print(estudiantesTelefono[i])
+            print("♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦")
+
+
+
+def mesas_electo_U():
+    #esta funcion nos ayudar a clasificar a los estudiantes que deben votar en sus respecivas mesas electorales en la elecciones universitarias
+    #Cada mesa de votacion abarcara 3 letras Ejm:A,B y C
+    print("♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥")
+    posicion2=estudiantesCI.index(input('Ingrese el Carnet de Identidad del estudiante:'))
+    apellido=estudiantesApellidos[posicion2]
+    letra=apellido[0]
+    letra_lower=letra.lower()
+    if letra=="A" or letra=="B" or letra=="C":
+        print("♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣")
+        print("El estudiante tendra el deber de votar en la Mesa N°1 porque su apellido Inicializa en:",letra)
+        print("♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣")
+    if letra=="D" or letra=="E" or letra=="F":
+        print("♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣")
+        print("El estudiante tendra el deber de votar en la Mesa N°2 porque su apellido Inicializa en:",letra)
+        print("♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣")
+    if letra=="G" or letra=="H" or letra=="I":
+        print("♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣")
+        print("El estudiante tendra el deber de votar en la Mesa N°3 porque su apellido Inicializa en:",letra)
+        print("♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣")
+    if letra=="J" or letra=="K" or letra=="L":
+        print("♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣")
+        print("El estudiante tendra el deber de votar en la Mesa N°4 porque su apellido Inicializa en:",letra)
+        print("♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣")
+    if letra=="M" or letra=="N" or letra=="Ñ":
+        print("♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣")
+        print("El estudiante tendra el deber de votar en la Mesa N°5 porque su apellido Inicializa en:",letra)
+        print("♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣")
+    if letra=="O" or letra=="P" or letra=="Q":
+        print("♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣")
+        print("El estudiante tendra el deber de votar en la Mesa N°6 porque su apellido Inicializa en:",letra)
+        print("♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣")
+    if letra=="R" or letra=="S" or letra=="T":
+        print("♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣")
+        print("El estudiante tendra el deber de votar en la Mesa N°7 porque su apellido Inicializa en:",letra)
+        print("♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣")
+    if letra=="U" or letra=="W" or letra=="V":
+        print("♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣")
+        print("El estudiante tendra el deber de votar en la Mesa N°8 porque su apellido Inicializa en:",letra)
+        print("♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣")
+    if letra=="X" or letra=="Y" or letra=="Z":
+        print("♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣")
+        print("El estudiante tendra el deber de votar en la Mesa N°9 porque su apellido Inicializa en:",letra)
+        print("♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣♣")
+
+
+while True:
+    mostrarMenu()
+    opcion=input(     "Ingrese alguna de las opciones del Menu:")
+    if opcion == '1':
+        print('♥Esta es la lista de estudiantes registrados:')
+        mostrarEstudiantes()
+    elif opcion == '2':
+        print('♥Ingrese los datos del estudiante nuevo:')
+        adicionarEstudiante()
+    elif opcion == '3':
+        print("♥Siga los siguientes pasos para la modificacion:")
+        modificarEstudiante()
+    elif opcion == '4':
+        print("♥Siga los siguientes pasos para eliminar a un estudiante:")
+        eliminarEstudiante()
+    elif opcion == '5':
+        print("♥Consulta la Salida en Cuarentena:")
+        consultaSalidaCuarentena()
+    elif opcion == '6':
+        print("♥Consulta de Mesa Electoral en elecciones U:")
+        mesas_electo_U()
+    else:
+        print('♥♥♥Gracias por utilizar el Sistema♥♥♥')
+        break
+    print('♥La tarea se ejecuto correctamente,¿Desea realizar alguna otra operacion?')
+
+# TODO: PERSISTENCIA+
+# TAREA1: Crear e implementar el metodo modificar de las listas de estudiantes
+# TAREA2: Inventar una consulta cualquiera (como consultaSalidaCuarentena) y ponerla como opcion 6
+# TAREA3: Mejorar el diseño en general
+# Esta tarea vale como 2 tareas (semana del 25 de junio y semana del 02 de julio)
+
+
